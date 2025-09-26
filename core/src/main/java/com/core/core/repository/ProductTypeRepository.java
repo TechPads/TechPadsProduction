2b17efb6a73e85a1e4795b876b34992dda1b0dc6
Jesuss2706 <jeslamu2006@gmail.com>
2025-09-26 17:05:13 -0500

    feat:  create repositories about product, productType and inventory

---
+++ core/src/main/java/com/core/core/repository/ProductTypeRepository.java
@@ -0,0 +1,10 @@
+package com.core.core.repository;
+
+
+import com.core.core.module.ProductType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductTypeRepository extends JpaRepository<ProductType, String> {
+
+
+}
