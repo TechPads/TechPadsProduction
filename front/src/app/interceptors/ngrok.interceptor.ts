4712799d14d1eb74d931325efe91a575e24fc1f0
Juan Henao <juanhenao1234@gmail.com>
2025-10-29 16:57:06 -0500

    fixing ngrok errors for deploy

--- front/src/app/interceptors/ngrok.interceptor.ts
+++ front/src/app/interceptors/ngrok.interceptor.ts
@@ -1,13 +1,10 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
-
 export const ngrokInterceptor: HttpInterceptorFn = (req, next) => {
-
   if (req.url.includes('ngrok')) {
     const modifiedReq = req.clone({
       setHeaders: {
-        'ngrok-skip-browser-warning': 'true',
-        'User-Agent': 'Angular-App'
+        'ngrok-skip-browser-warning': '69420', 
       }
     });
     return next(modifiedReq);
