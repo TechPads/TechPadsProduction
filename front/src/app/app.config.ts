44ca6bb3fb353380044deb7b613f5a6eeb01f36f
Juan Henao <juanhenao1234@gmail.com>
2025-10-28 23:06:40 -0500

    add ngrok interceptor

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -2,13 +2,17 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { ngrokInterceptor } from './interceptors/ngrok.interceptor';  
 import { routes } from './app.routes'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([AuthInterceptor])
+      withInterceptors([
+        ngrokInterceptor,   
+        AuthInterceptor     
+      ])
     )
   ]
 };
