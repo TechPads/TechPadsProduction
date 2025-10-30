28e31c6e64245c90c970947205d916aa8fb41941
Jesuss2706 <jeslamu2006@gmail.com>
2025-10-29 21:24:33 -0500

    feat(back): changes with new table camps in product and inventory

--- core/src/main/java/com/core/core/repository/ProductRepository.java
+++ core/src/main/java/com/core/core/repository/ProductRepository.java
@@ -6,32 +6,37 @@ import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // Crear producto (ya no enviamos proCode)
     @Procedure(procedureName = "GESTION_PRODUCTO.CREAR_PRODUCTO")
     void crearProducto(
-            @Param("v_proCode") Long proCode,
             @Param("v_proName") String proName,
             @Param("v_proImg") String proImg,
-            @Param("v_proPrice") java.math.BigDecimal proPrice,
+            @Param("v_proPrice") BigDecimal proPrice,
             @Param("v_proType") Long proType,
             @Param("v_descript") String descript,
-            @Param("v_proMark") String proMark
+            @Param("v_proMark") String proMark,
+            @Param("v_status") String status
     );
 
+    // Modificar producto (sí requiere proCode)
     @Procedure(procedureName = "GESTION_PRODUCTO.MODIFICAR_PRODUCTO")
     void modificarProducto(
             @Param("v_proCode") Long proCode,
             @Param("v_proName") String proName,
             @Param("v_proImg") String proImg,
-            @Param("v_proPrice") java.math.BigDecimal proPrice,
+            @Param("v_proPrice") BigDecimal proPrice,
             @Param("v_proType") Long proType,
             @Param("v_descript") String descript,
-            @Param("v_proMark") String proMark
+            @Param("v_proMark") String proMark,
+            @Param("v_status") String status
     );
 
-
+    // Eliminar producto (borrado lógico)
     @Procedure(procedureName = "GESTION_PRODUCTO.ELIMINAR_PRODUCTO")
     void eliminarProducto(@Param("v_proCode") Long proCode);
 }
